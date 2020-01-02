72402cf0010e3516d45e2f701610305091870c5d
Liam Pulsifer <lwpulsifer@gmail.com>
2020-01-02 14:29:07 -0500

    Broke off markdown creation into a separate file called log_table, then imported it in main to write the markdown

---
+++ log_table.py
@@ -0,0 +1,84 @@
+import datetime
+
+class LogTable():
+
+    def __init__(self, start_day, columns=10, days=100):
+        self.columns = columns
+        self.days = days
+        self.start_day = start_day
+        self.table = self.gen_table()
+    
+    def get_intro(self):
+        intro = f"""# {self.days} Days Of Code - Log   
+*Main Commitment*:
+
+I will code in **YOUR LANGUAGE** programming language for at least an hour every day for the next 100 days.
+
+Start Date: {self.start_day.strftime("%B %d, %Y")}
+End Date (without any breaks): {(self.start_day + datetime.timedelta(self.days)).strftime("%B %d, %Y")}
+
+----
+"""
+        return intro
+    
+    def get_header(self):
+        header = [["| Days |"]]
+        header[-1].extend([f" {str(i)} |" for i in range(1, self.columns + 1)])
+        header.append([f"| -- | " for i in range(self.columns + 1)])
+        return header
+
+    def gen_table(self):
+        day_count = 1
+        table = []
+        table.extend(self.get_header())
+        while day_count <= self.days:
+            row_counter = 0
+            table.append([])
+            table[-1].append(f"| {day_count - 1:02d} |")
+            while row_counter < self.columns:
+                day = self.start_day + datetime.timedelta(day_count - 1)
+                day_format = day.strftime("%B-%d-%Y")
+                table[-1].append(f"[Day {day_count}](#day-{day_count}-{day_format.lower()}) | ")
+                row_counter += 1
+                day_count += 1
+                if day_count > self.days:
+                    break
+        return table
+    
+    def get_diary(self):
+        diary = []
+        day_count = 1
+        while day_count <= self.days:
+            day = self.start_day + datetime.timedelta(day_count - 1)
+            day_format = day.strftime("%B %d, %Y")
+            diary.append(f"""
+### Day {day_count}: {day_format}
+
+**Today's Progress**:
+
+**Thoughts:**
+
+**Link(s) to work**: [Example](http://www.example.com)
+
+[Back to Top](#days)
+
+----""")
+            day_count += 1
+        return "".join(diary)
+
+    
+    def get_string_table(self):
+        string_table = ""
+        for row in self.table:
+            string_table += "".join(row)
+            string_table += "\n"
+        string_table += "\n"
+        return string_table
+
+    def write_table(self, filename):
+        with open("test.md", "w") as write_file:
+            write_file.write(self.get_string_table())
+
+if __name__ == '__main__':
+    lt = LogTable(datetime.date.today(), columns=13, days=144)
+    lt.write_table("test.md")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 import os.path
 from argparse import ArgumentParser
+from log_table import LogTable
 
 
 class ErrorFormatDate(Exception):
@@ -52,61 +53,10 @@ if os.path.isfile(filename) and args.overwrite:
 else:
     create = "x"
 
-with open(filename, create) as f:
-    f.write(f"""# 100 Days Of Code - Log
+with open(args.filename, create) as f:
+        log_table = LogTable(start_day, days=args.duration)
+        f.write(log_table.get_intro())
+        f.write(log_table.get_string_table())
+        f.write(log_table.get_diary())
+        f.write(f"\n\n\n# Conclusion\n\n")
 
-*Main Commitment*:
-
-I will code in **YOUR LANGUAGE** programming language for at least an hour every day for the next 100 days.
-
-Start Date: **{start_day.strftime("%B %d, %Y")}**\n
-End Date (without any breaks): **{(start_day + datetime.timedelta(100)).strftime("%B %d, %Y")}**
-
-----
-## Days:
-
-""")
-
-    header = "|+|"
-    header += "".join([f"{i:02d}|" for i in range(0, duration, 10)])
-    header += "\n|--|"
-    header += "".join(["--|" for i in range(0, duration, 10)])
-    header += "\n"
-    f.write(header)
-
-    time_var = start_day
-    for i in range(10):
-        f.write(f"|{i:02d}|")
-        for j in range(1, duration, 10):
-            day_format = time_var.strftime("%B-%d-%Y")
-            f.write(f"[Day {i+j}](#day-{i+j}-{day_format.lower()}) | ")
-            time_var += datetime.timedelta(10)
-        if i+j == duration-1:
-            f.write(
-                f"[Day {duration}](#day-{duration}-{day_format.lower()}) | ")
-        f.write("\n")
-        time_var -= datetime.timedelta(duration-1)
-
-    f.write(f"\n[Or Jump Right To Conclusion!](#Conclusion)\n")
-
-    time_var = start_day
-    for i in range(1, duration+1):
-        day_format = time_var.strftime("%B, %d, %Y")
-        f.write(f"""
-### Day {i}: {day_format}
-
-**Today's Progress**:
-
-**Thoughts:**
-
-**Link(s) to work**
-[Example](http://www.example.com)
-
-[Back Top](#days)
-
-----""")
-        time_var += datetime.timedelta(1)
-
-    f.write(f"\n\n\n# Conclusion\n\n")
-
-print(args)
