bcea0c2ab5d5cbbf57608872a5f64c4e01930a3d
MattiooFR <13595614+MattiooFR@users.noreply.github.com>
2020-01-02 19:01:24 +0100

    feat: added -o, -v, -d, -l, -f arguments, -v & -d works.

--- main.py
+++ main.py
@@ -14,13 +14,25 @@ A log.md with the date of today will still be created in case this is what you a
 parser = ArgumentParser()
 parser.add_argument("date", nargs='?', default="",
                     help='Set your own starting date : 22/12/2019, yesterday, or\
-                         "20 of december 2019" in any language. Default is today.')
+                         "20 of december 2019" in any language. Default is today')
+parser.add_argument("-o", "--overwrite", help='overwrite the existing log.md file',
+                    action="store_true")
+parser.add_argument("-f", "--filename", help='choose your own filename output',
+                    type=str, default="log.md")
+parser.add_argument("-l", "--list", help='append this log to an existing list of logs',
+                    action="store_true")
+parser.add_argument("-d", "--duration", help='set a custom duration for your challenge',
+                    default=100, type=int)
+parser.add_argument("-v", "--verbose", help='print process infos',
+                    action="store_true")
 args = parser.parse_args()
 
 if args.date and isDateparserLoaded:
     start_day = dateparser.parse(args.date)
 else:
     start_day = datetime.date.today()
+
+duration = args.duration
 
 with open("log.md", "x") as f:
     f.write(f"""# 100 Days Of Code - Log
@@ -29,30 +41,38 @@ with open("log.md", "x") as f:
 
 I will code in **YOUR LANGUAGE** programming language for at least an hour every day for the next 100 days.
 
-Start Date: {start_day.strftime("%B %d, %Y")}
-End Date (without any breaks): {(start_day + datetime.timedelta(100)).strftime("%B %d, %Y")}
+Start Date: **{start_day.strftime("%B %d, %Y")}**\n
+End Date (without any breaks): **{(start_day + datetime.timedelta(100)).strftime("%B %d, %Y")}**
 
 ----
 ## Days:
-|+|00                                    |10                                   |20                                 |30                                  |40                                  |50                                  |60                                  |70                                  |80                                  |90                                    |
-|--|-------------------------------------|-------------------------------------|-----------------------------------|------------------------------------|------------------------------------|------------------------------------|------------------------------------|------------------------------------|------------------------------------|--------------------------------------|
+
 """)
+
+    header = "|+|"
+    header += "".join([f"{i:02d}|" for i in range(0, duration, 10)])
+    header += "\n|--|"
+    header += "".join(["--|" for i in range(0, duration, 10)])
+    header += "\n"
+    f.write(header)
+
     time_var = start_day
     for i in range(10):
         f.write(f"|{i:02d}|")
-        for j in range(1, 100, 10):
+        for j in range(1, duration, 10):
             day_format = time_var.strftime("%B-%d-%Y")
             f.write(f"[Day {i+j}](#day-{i+j}-{day_format.lower()}) | ")
             time_var += datetime.timedelta(10)
-        if i+j == 99:
-            f.write(f"[Day 100](#day-100-{day_format.lower()}) | ")
+        if i+j == duration-1:
+            f.write(
+                f"[Day {duration}](#day-{duration}-{day_format.lower()}) | ")
         f.write("\n")
-        time_var -= datetime.timedelta(99)
+        time_var -= datetime.timedelta(duration-1)
 
     f.write(f"\n[Or Jump Right To Conclusion!](#Conclusion)\n")
 
     time_var = start_day
-    for i in range(1, 101):
+    for i in range(1, duration+1):
         day_format = time_var.strftime("%B, %d, %Y")
         f.write(f"""
 ### Day {i}: {day_format}
@@ -70,3 +90,5 @@ End Date (without any breaks): {(start_day + datetime.timedelta(100)).strftime("
         time_var += datetime.timedelta(1)
 
     f.write(f"\n\n\n# Conclusion\n\n")
+
+print(args)
